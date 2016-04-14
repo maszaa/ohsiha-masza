77f1a18d8219d8757b61e61ae1141400f33ae30a
Samuli Kohomäki <samuli.kohomaki@student.tut.fi>
2016-04-14 14:23:35 +0300

    User can now open his/her private documents in site or as json, little modfications to other things too

--- app/routes/document.js
+++ app/routes/document.js
@@ -1,75 +1,64 @@
 module.exports = function(app, models, checkLogin) {
   app.get('/document/:id', function(req, res, next) {
-    /*if (req.isAuthenticated()) {
-      if (req.query.format === 'json') {
-        models.Document.findOne({_id: req.params.id, user: req.user.username}).lean().exec(function(err, document) {
-          if (err) {
-            console.log(err);
-            res.status(404).json(err);
-          }
-          else if (document === null) {
-            res.status(404).json({error: 'Artikkelia ei löytynyt'});
-          }
-          else {
-            if(req.isAuthenticated() && document.user === req.user.username) {
+    if (req.query.format === 'json') {
+      models.Document.findOne({_id: req.params.id}).lean().exec(function(err, document) {
+        if (err) {
+          console.log(err);
+          res.status(404).json(err);
+        }
+        else if (document === null) {
+          res.status(404).json({error: 'Artikkelia ei löytynyt'});
+        }
+        else {
+          if (req.isAuthenticated()) {
+            if (document.user === req.user.username) {
               res.json(document);
-            }
-            else if(req.isAuthenticated() && document.user !== req.user.username && document.private === true) {
-              res.status(404).json({error: 'Artikkeli on yksityinen'});
             }
             else if (document.private === false) {
               res.json(document);
             }
+            else {
+              res.status(403).json({error: 'Artikkeli ei sallittu'});
+            }
           }
-          else {
+          else if (document.private === false) {
             res.json(document);
           }
-        });
-      }
-      else {
-        models.Document.findOne({_id: req.params.id, user: req.user.username}, function(err, document) {
-          if (err) {
-            console.log(err);
-            res.render('document', {title: err, documents: {}});
-          }
-          else if (document === null) {
-            res.render('document', {title: 'Artikkelia ei löytynyt'})
-          }
           else {
-            res.render('document', {title: document.title, document: document});
+            res.status(403).json({error: 'Artikkeli ei sallittu'});
           }
-        });
-      }
+        }
+      });
     }
-    else {*/
-      if (req.query.format === 'json') {
-        models.Document.findOne({private: false, _id: req.params.id}).lean().exec(function(err, document) {
-          if (err) {
-            console.log(err);
-            res.status(404).json(err);
+    else {
+      models.Document.findOne({_id: req.params.id}, function(err, document) {
+        if (err) {
+          console.log(err);
+          res.render('document', {title: err});
+        }
+        else if (document === null) {
+          res.render('document', {title: 'Artikkelia ei löytynyt'})
+        }
+        else {
+          if (req.isAuthenticated()) {
+            if (document.user === req.user.username) {
+              res.render('document', {title: document.title, document: document});
+            }
+            else if (document.private === false) {
+              res.render('document', {title: document.title, document: document});
+            }
+            else {
+              res.render('document', {title: 'Artikkeli ei sallittu'});
+            }
           }
-          else if (document === null) {
-            res.status(404).json({error: 'Artikkelia ei löytynyt'});
-          }
-          else {
-            res.json(document);
-          }
-        });
-      }
-      else {
-        models.Document.findOne({private: false, _id: req.params.id}, function(err, document) {
-          if (err) {
-            console.log(err);
-            res.render('document', {title: err, documents: {}});
-          }
-          else if (document === null) {
-            res.render('document', {title: 'Artikkelia ei löytynyt'})
-          }
-          else {
+          else if (document.private === false) {
             res.render('document', {title: document.title, document: document});
           }
-        });
-      }
-    //}
+          else {
+            res.render('document', {title: 'Artikkeli ei sallittu'});
+          }
+        }
+      });
+    }
   });
 };
